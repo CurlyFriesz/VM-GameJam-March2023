6e4e79acd5ecf1a1a7cd4e179ead4deb8d63c8d5
Jad Menkara <Jadmenkara@gmail.com>
2023-03-13 22:24:19 -0400

    Update playerController.cs

--- Interstellar Odyssey/Assets/Scripts/playerController.cs
+++ Interstellar Odyssey/Assets/Scripts/playerController.cs
@@ -11,17 +11,17 @@ public class playerController : MonoBehaviour
     public Vector2 rotation;
     public float sensitivity;
     public float speed;
+    public Transform camTransform;
 
     //movement
     public Rigidbody rigidbodyCam;
     public float jetRotateSpeed;
+    Vector3 previousVelocity;
 
     //jet spin
     public Transform jetBody;
     public float jetSensitivity;
     public float jetStop;
-
-
 
     //remember to use *Time.deltaTime;
 
@@ -30,42 +30,41 @@ public class playerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;   
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void jetBodyLerp(float z)
+    {
+        Vector3 direction = new Vector3(jetBody.rotation.eulerAngles.x, jetBody.rotation.eulerAngles.y, z);
+        Quaternion targetRotation = Quaternion.Euler(direction);
+        jetBody.rotation = Quaternion.Lerp(jetBody.rotation, targetRotation, Time.deltaTime * 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 
-        //input values
+        Vector3 rigidbodyAcceleration = (rigidbodyCam.velocity - previousVelocity) / Time.fixedDeltaTime;
+
+        camTransform.position = new Vector3(camTransform.position.x, camTransform.position.y - rigidbodyAcceleration.y, camTransform.position.z);
+
+        //Rotates vehicle up and down
         rotation.y -= Input.GetAxis("Mouse Y") * jetRotateSpeed;
         rotation.y = Mathf.Clamp(rotation.y, -70, 70);
 
 
+        //Rotates vehicle left and right
+        if (Input.GetAxis("Horizontal") > 0) jetBodyLerp(-jetStop);
 
-        if (Input.GetAxis("Horizontal") > 0)
-        {
-            Vector3 direction = new Vector3(jetBody.rotation.eulerAngles.x, jetBody.rotation.eulerAngles.y, -jetStop);
-            Quaternion targetRotation = Quaternion.Euler(direction);
-            jetBody.rotation = Quaternion.Lerp(jetBody.rotation, targetRotation, Time.deltaTime * 1);
-        }
-        else if (Input.GetAxis("Horizontal") < 0)
-        {
-            Vector3 direction = new Vector3(jetBody.rotation.eulerAngles.x, jetBody.rotation.eulerAngles.y, jetStop);
-            Quaternion targetRotation = Quaternion.Euler(direction);
-            jetBody.rotation = Quaternion.Lerp(jetBody.rotation, targetRotation, Time.deltaTime * 1);
-        }
-        else
-        {
-            Vector3 direction = new Vector3(jetBody.rotation.eulerAngles.x, jetBody.rotation.eulerAngles.y, 0);
-            Quaternion targetRotation = Quaternion.Euler(direction);
-            jetBody.rotation = Quaternion.Lerp(jetBody.rotation, targetRotation, Time.deltaTime * 1);
-        }
+        else if (Input.GetAxis("Horizontal") < 0) jetBodyLerp(jetStop);
 
+        else jetBodyLerp(0);
+
+
+        //Vehicle movement
         rigidbodyCam.velocity = transform.forward * speed + transform.right * Input.GetAxis("Horizontal") * speed;
 
         transform.localRotation = Quaternion.Euler(rotation.y, rotation.x, transform.rotation.z);
-        
-    
-}
+
+        previousVelocity = rigidbodyCam.velocity;
+}       
 }
